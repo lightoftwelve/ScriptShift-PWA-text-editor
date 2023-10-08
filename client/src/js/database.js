1485df59509fe2eeab3a9c1c1fd90d86831375dd
lightoftwelve <info@lightoftwelve.com>
2023-10-08 19:03:28 -0400

    updated database.js to include putDB and getDB as well as renaming

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,22 +1,35 @@
 import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB("jate", 1, {
+  openDB("scriptshift", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains("jate")) {
-        console.log("jate database already exists");
+      if (db.objectStoreNames.contains("scriptshift")) {
+        console.log("scriptshift database already exists");
         return;
       }
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
-      console.log("jate database created");
+      db.createObjectStore("scriptshift", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+      console.log("scriptshift database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error("putDb not implemented");
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction("scriptshift", "readwrite");
+  const store = tx.objectStore("scriptshift");
+  await store.put({ content });
+  return tx.done;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction("scriptshift", "readonly");
+  const store = tx.objectStore("scriptshift");
+  const allContent = await store.getAll();
+  return allContent;
+};
 
 initdb();
 
