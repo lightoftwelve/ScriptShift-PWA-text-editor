a3c258ec488eff32a64fec705df80b67d725f19a
lightoftwelve <info@lightoftwelve.com>
2023-10-08 18:44:56 -0400

    updated settings on webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
@@ -17,10 +14,58 @@ module.exports = () => {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
-    plugins: [],
+    plugins: [
+      // Generates an HTML file from a template and injects bundles.
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "index.html",
+      }),
 
+      // Uses custom service worker for Workbox.
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+      // Generates a manifest.json for PWA capabilities.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "ScriptShift Text Editor",
+        short_name: "ScriptShift",
+        description: "A robust text editor for the modern web.",
+        background_color: "#ffffff",
+        theme_color: "#333333",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+    ],
     module: {
-      rules: [],
+      rules: [
+        {
+          // Handles CSS imports and injects them into the DOM.
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          // Transpiles ES6+ to ES5.
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+      ],
     },
   };
 };
